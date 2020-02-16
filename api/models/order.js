0db5660a37a4163d214624cda17aeeb179f10482
Ken Dong <yid164@mail.usask.ca>
2020-02-16 11:36:28 -0600

    implemented order schema

--- api/models/order.js
+++ api/models/order.js
@@ -6,6 +6,8 @@ const orderSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     customerEmail: {type: String},
     date: {type: Date},
+    inventoryItem: {type:mongoose.Types.ObjectId, ref: 'Inventory', required: true},
+    orderQuantity:{type: Number, required: true},
     orderStatus: {type: String}
 });
 
