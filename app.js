e313c3e376a6bee0876860f2b49c725c2b227e6b
Ken Dong <yid164@mail.usask.ca>
2020-02-15 23:08:01 -0600

    use the body-parser to encode json

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express();
 
 const morgen = require('morgan');
 
-
+const bodyParser = require('body-parser');
 
 const inventoryRoutes = require('./api/routes/inventories');
 
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgen('dev'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(bodyParser.json());
 
 app.use('/inventories', inventoryRoutes);
 
@@ -20,7 +24,7 @@ app.use((req, res, next)=>{
     const error = new Error('Not Found Error');
     error.status(404);
     next(error);
-})
+});
 
 app.use((error, req, res, next)=>{
     res.status(error.status||500);
