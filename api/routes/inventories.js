64ff776ea8d112f6a9a004f810dcee3af0630f29
Ken Dong <yid164@mail.usask.ca>
2020-02-15 22:02:21 -0600

    update routes

--- api/routes/inventories.js
+++ api/routes/inventories.js
@@ -5,22 +5,46 @@ const router = express.Router();
 /**
  * Get and Post all inventory items
  */
-router.get('/');
+router.get('/',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Get /inventroies'
+    })
+});
 
-router.post('/');
+router.post('/',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Post /inventroies'
+    })
+});
 
 
 
 /**
  * Get, Post, Put, and Delete a signle inventory item
  */
-router.post('/:itemId');
+router.post('/:itemId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Post /inventroies/itemId'
+    })
+});
 
-router.get('/:itemId');
+router.get('/:itemId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Get /inventroies/itemId'
+    })
+});
 
-router.put('/:itemId');
+router.put('/:itemId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Put /inventroies/itemId'
+    })
+});
 
-router.delete('/itemId');
+router.delete('/itemId', (req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Delete /inventroies/itemId'
+    })
+});
 
 
 module.exports = router;
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,22 +5,46 @@ const router = express.Router();
 /**
  * Get and Post all all orders
  */
-router.get('/');
+router.get('/',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Get /orders'
+    })
+});
 
-router.post('/');
+router.post('/',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Post /orders'
+    })
+});
 
 
 
 /**
  * Get, Post, Put, and Delete a signle order
  */
-router.post('/:orderId');
+router.post('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Post /orders/orderId'
+    })
+});
 
-router.get('/:orderId');
+router.get('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Get /orders/orderId'
+    })
+});
 
-router.put('/:orderId');
+router.put('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Put /orders/orderId'
+    })
+});
 
-router.delete('/orderId');
+router.delete('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling Delete /orders/orderId'
+    })
+});;
 
 
 module.exports = router;
