d39dcb96a8b3b23c67d58f6cf5c6e83727f73ebe
Ken Dong <yid164@mail.usask.ca>
2020-02-15 23:04:55 -0600

    setup the printed error

--- app.js
+++ app.js
@@ -17,9 +17,18 @@ app.use('/inventories', inventoryRoutes);
 app.use('/orders', orderRoutes)
 
 app.use((req, res, next)=>{
-    res.status(200).json({
-        message: 'It works'
-    });
+    const error = new Error('Not Found Error');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status||500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
 });
 
 module.exports = app;
