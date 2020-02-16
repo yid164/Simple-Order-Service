af9ec00cad920de7fdbdf8293cfc30dcb45dac78
Ken Dong <yid164@mail.usask.ca>
2020-02-15 23:14:49 -0600

    Added CORS for RESTfulAPI

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
 
+app.use(((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, UPDATE, DELETE, GET');
+        return res.status(200).json({});
+    
+    }
+    next();
+}))
+
 app.use('/inventories', inventoryRoutes);
 
 app.use('/orders', orderRoutes)
