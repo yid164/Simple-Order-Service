43cca47a7333c6f3706eb4b6206308f72300fd4b
Ken Dong <yid164@mail.usask.ca>
2020-02-15 21:25:39 -0600

    add routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const inventoryRoutes = require('./api/routes/inventories');
+
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/inventories', inventoryRoutes);
+
+app.use('/orders', orderRoutes)
+
 app.use((req, res, next)=>{
     res.status(200).json({
         message: 'It works'
