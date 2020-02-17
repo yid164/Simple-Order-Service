40e79f1e30caeea649ad8f9c4c1cce4069ffcc16
Ken Dong <yid164@mail.usask.ca>
2020-02-16 21:11:27 -0600

    Simple endpoint testing of inventtoies done

--- test/api/TestCases/test.js
+++ test/api/TestCases/test.js
@@ -8,7 +8,7 @@ const app = require('../../../app');
 /**
  * POST /inventories
  */
-it('POST /inventoreis,... OK, creating a new inventoiris', async ()=>{
+it('POST /inventoreis/,... OK, creating a new inventoiris', async ()=>{
     request(app).post('/inventories')
     .send({ name: 'ITEM 2', price: '22.99', description: 'THIS IS ITEM 1', quantity: '20'})
     .then((res)=>{
@@ -29,7 +29,7 @@ it('POST /inventoreis,... OK, creating a new inventoiris', async ()=>{
  * GET /inventoreis
  */
 
-it('GET /inventoreis,... OK, get all inventoiris', async ()=>{
+it('GET /inventoreis/,... OK, get all inventoiris', async ()=>{
     request(app).get('/inventories')
     .then((res)=>{
         const body = res.body;
@@ -48,7 +48,7 @@ it('GET /inventoreis,... OK, get all inventoiris', async ()=>{
 /**
  * GET /inventoreis/items
  */
-it('GET /inventory Items,... OK, get one item from inventoiris', async ()=>{
+it('GET /inventories/items,... OK, get one item from inventoiris', async ()=>{
     request(app).get('/inventories/5e48dc640ab53f0aa918c296')
     .then((res)=>{
         const body = res.body;
@@ -61,4 +61,38 @@ it('GET /inventory Items,... OK, get one item from inventoiris', async ()=>{
     }).catch(err=>{
         error: err
     });
+});
+
+/**
+ * Update inventory item
+ */
+
+it('Update PUT /inventoreis/id ,... OK, updating inventories item id: 5e48dc640ab53f0aa918c296 ', async ()=>{
+    request(app).put('/inventories/5e48dc640ab53f0aa918c296')
+    .send([{ name: 'ITEM 4', price: '12.99', description: 'THIS IS ITEM 4', quantity: '8'}])
+    .then((res)=>{
+        const body = res.body;
+        expect(body).to.contain.property('ok');
+        expect(body).to.contain.property('n');
+        expect(body).to.contain.property('nModified');
+        done();
+    }).catch(err=>{
+        error: err
+    });
+
+});
+
+/**
+ * Delete inventory item
+ */
+it('DELELTE /inventoreis/id ,... OK, Deleting inventories item id: 5e48ea6cecd7c80c3b4f8f72 ', async ()=>{
+    request(app).put('/inventories/5e48ea6cecd7c80c3b4f8f72')
+    .then((res)=>{
+        const body = res.body;
+        expect(body.message).to('Delete inventory item successful');
+        done();
+    }).catch(err=>{
+        error: err
+    });
+
 });
